53990370ab3425ca48659930a4ad628b2a6d68f5
Gabriel Simmer <gsimmer@protonmail.com>
2023-05-05 22:14:25 +0100

    Minor formatting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,17 @@
 <script>
-  import '../app.css';
-  import { enableAnalytics } from '$lib/analytics';
+	import '../app.css';
+	import { enableAnalytics } from '$lib/analytics';
 </script>
 
 <svelte:head>
-  <title>Fursona Lookup</title>
-  <link
-    rel="icon"
-    href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏳️‍🌈</text></svg>"
+	<title>Fursona Lookup</title>
+	<link
+		rel="icon"
+		href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏳️‍🌈</text></svg>"
 	/>
-{#if enableAnalytics}
-  <script defer data-domain="fursona.gmem.ca" src="https://plausible.io/js/plausible.js"></script>
-{/if}
+	{#if enableAnalytics}
+		<script defer data-domain="fursona.gmem.ca" src="https://plausible.io/js/plausible.js"></script>
+	{/if}
 </svelte:head>
 
 <div class="bg-white dark:bg-slate-800">
